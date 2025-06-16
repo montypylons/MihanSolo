532961d095e199d7de893a70f5647bbd9c1f6b62
Mihin Benaragama <mbenaragama@nastudents.org>
2025-06-16 18:16:45 -0400

    add main

---
+++ main.py
@@ -0,0 +1,11 @@
+import pychess
+def main():
+    pychess.start() 
+    # add more stuff here
+
+if __name__ == "__main__":
+    main()
+
+def search():
+    # This function is a placeholder for future search functionality
+    pass
